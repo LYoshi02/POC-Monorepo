5ccddbaa5a738f7380532df19b80e6f3c89d23f3
Yoshi Debat <yoshiwebdev@gmail.com>
2024-02-18 20:29:18 -0300

    fix client test

--- apps/client/src/App.test.tsx
+++ apps/client/src/App.test.tsx
@@ -5,7 +5,7 @@ import { render, screen } from "@testing-library/react";
 it("renders Vite heading", () => {
   render(<App />);
 
-  const viteHeading = screen.getByRole("heading", { name: /vite/i });
+  const viteHeading = screen.getByRole("heading", { name: /notes/i });
 
   expect(viteHeading).toBeInTheDocument();
 });
